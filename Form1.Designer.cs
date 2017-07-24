5633c4c9c0244dc4be8ee118a62369b21a119b61
Ryan Olds <ryanrolds@gmail.com>
2017-07-24 08:03:22 -0700

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,246 @@
+﻿namespace Classifier {
+    partial class Form1 {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing) {
+            if (disposing && (components != null)) {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent() {
+            this.menuStrip1 = new System.Windows.Forms.MenuStrip();
+            this.fileToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.loadImagesToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button10 = new System.Windows.Forms.Button();
+            this.menuStrip1.SuspendLayout();
+            this.statusStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            this.menuStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.fileToolStripMenuItem});
+            this.menuStrip1.Location = new System.Drawing.Point(0, 0);
+            this.menuStrip1.Name = "menuStrip1";
+            this.menuStrip1.Size = new System.Drawing.Size(584, 24);
+            this.menuStrip1.TabIndex = 0;
+            this.menuStrip1.Text = "menuStrip1";
+            // 
+            // fileToolStripMenuItem
+            // 
+            this.fileToolStripMenuItem.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.loadImagesToolStripMenuItem});
+            this.fileToolStripMenuItem.Name = "fileToolStripMenuItem";
+            this.fileToolStripMenuItem.Size = new System.Drawing.Size(37, 20);
+            this.fileToolStripMenuItem.Text = "File";
+            // 
+            // loadImagesToolStripMenuItem
+            // 
+            this.loadImagesToolStripMenuItem.Name = "loadImagesToolStripMenuItem";
+            this.loadImagesToolStripMenuItem.Size = new System.Drawing.Size(141, 22);
+            this.loadImagesToolStripMenuItem.Text = "Load Images";
+            this.loadImagesToolStripMenuItem.Click += new System.EventHandler(this.updateImageDir);
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 489);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(584, 22);
+            this.statusStrip1.TabIndex = 2;
+            this.statusStrip1.Text = "Ready";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(39, 17);
+            this.toolStripStatusLabel1.Text = "Ready";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.Location = new System.Drawing.Point(0, 27);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(584, 323);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.CenterImage;
+            this.pictureBox1.TabIndex = 3;
+            this.pictureBox1.TabStop = false;
+            // 
+            // listView1
+            // 
+            this.listView1.Alignment = System.Windows.Forms.ListViewAlignment.Left;
+            this.listView1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.listView1.AutoArrange = false;
+            this.listView1.HideSelection = false;
+            this.listView1.Location = new System.Drawing.Point(0, 356);
+            this.listView1.MultiSelect = false;
+            this.listView1.Name = "listView1";
+            this.listView1.ShowGroups = false;
+            this.listView1.Size = new System.Drawing.Size(584, 130);
+            this.listView1.TabIndex = 1;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.ItemSelectionChanged += new System.Windows.Forms.ListViewItemSelectionChangedEventHandler(this.itemSelectionChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(8, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "label1";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(12, 58);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 30);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "SL";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.imageButtonClicked);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(48, 58);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 30);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "F";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.imageButtonClicked);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(84, 58);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(30, 30);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "SR";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.imageButtonClicked);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(84, 94);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(30, 30);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "R";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.imageButtonClicked);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 94);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(30, 30);
+            this.button6.TabIndex = 8;
+            this.button6.Text = "L";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.imageButtonClicked);
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(48, 130);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(30, 30);
+            this.button8.TabIndex = 12;
+            this.button8.Text = "B";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.imageButtonClicked);
+            // 
+            // button10
+            // 
+            this.button10.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.button10.Location = new System.Drawing.Point(497, 35);
+            this.button10.Name = "button10";
+            this.button10.Size = new System.Drawing.Size(75, 23);
+            this.button10.TabIndex = 14;
+            this.button10.Text = "Bad";
+            this.button10.UseVisualStyleBackColor = true;
+            this.button10.Click += new System.EventHandler(this.imageButtonClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(584, 511);
+            this.Controls.Add(this.button10);
+            this.Controls.Add(this.button8);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.listView1);
+            this.Controls.Add(this.menuStrip1);
+            this.MainMenuStrip = this.menuStrip1;
+            this.MinimumSize = new System.Drawing.Size(600, 550);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.menuStrip1.ResumeLayout(false);
+            this.menuStrip1.PerformLayout();
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private System.Windows.Forms.ToolStripMenuItem fileToolStripMenuItem;
+        private System.Windows.Forms.ToolStripMenuItem loadImagesToolStripMenuItem;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button10;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Classifier {
+    public partial class Form1 : Form {
+        String imageDir = null;
+        String imageFile = null;
+        Boolean toldDone = false;
+
+        public Form1() {
+            InitializeComponent();
+
+            loadImages(".\\");
+        }
+
+        private void loadImages(String dir) {
+            imageDir = dir;
+
+            IEnumerable<string> files = Directory.EnumerateFiles(dir, "*.ppm");
+
+            // Show some detials in status bar
+            statusStrip1.Invalidate();
+            statusStrip1.Refresh();
+
+            ImageList images = new ImageList();
+            images.ImageSize = new Size(64, 64);
+
+            listView1.Clear(); // Clear existing items
+            listView1.LargeImageList = images;
+            
+            foreach (String file in files) {
+                String filename = Path.GetFileName(file);
+                PixelMap pix = new PixelMap(file);
+
+                Image img = Image.FromHbitmap(pix.BitMap.GetHbitmap());
+                images.Images.Add(filename, img);
+                ListViewItem viewItem = listView1.Items.Add(filename);
+
+                viewItem.Tag = getClassTag(pix);
+
+                viewItem.ImageKey = filename;
+            }
+
+            moveListToNext();
+        }
+
+        private object getClassTag(PixelMap pix) {
+            PixelMap.PixelMapHeader header = pix.Header;
+
+            String tag = null;
+            foreach(String comment in header.Comments) {
+                if (comment.IndexOf("Class: ") != -1) {
+                    tag = comment.Substring(8);
+                    break;                        
+                }
+            }
+
+            return tag;
+        }
+
+        private void updateImageDir(object sender, EventArgs e) {
+            FolderBrowserDialog diag = new FolderBrowserDialog();
+            DialogResult result = diag.ShowDialog();
+
+            if (result == DialogResult.OK) {
+                loadImages(diag.SelectedPath);
+            }
+        }
+
+        private void itemSelectionChanged(object sender, ListViewItemSelectionChangedEventArgs e) {
+            imageFile = e.Item.ImageKey;
+            label1.Text = imageDir + "\\" + imageFile;
+
+            pictureBox1.Image = new PixelMap(imageDir + "\\" + imageFile).BitMap;
+
+            setSelectedAction(e.Item.Tag);
+        }
+
+        private void setSelectedAction(object action) {
+            foreach (Control control in this.Controls) {
+                if (control.GetType() == typeof(Button)) {
+                    if (control.Text == (String)action) {
+                        control.BackColor = Color.Tomato;
+                    } else {
+                        control.BackColor = Button.DefaultBackColor;
+                    }
+                }
+            }
+        }
+
+        private void imageButtonClicked(object sender, EventArgs e) {
+            if (imageFile == null || imageDir == null) {
+                return;
+            }
+
+            String classification = ((Button)sender).Text;           
+
+            // Write action to image comments
+            int index = listView1.SelectedIndices[0];
+            listView1.Items[index].Tag = classification; // Tag list view item for searching
+
+            String fromFile = imageDir + "\\" + listView1.Items[index].ImageKey;
+            PixelMap pix = new PixelMap(fromFile);
+
+            List<String> comments = new List<string>();
+            comments.Add("Class: " + classification);
+            comments.Add("Hostname: " + System.Environment.MachineName.ToString());
+            comments.Add("Date: " + DateTime.Now.ToUniversalTime().ToString());
+
+            PixelMap.PixelMapHeader header = pix.Header;
+            header.Comments = comments;
+            pix.Header = header;
+
+            String toFile = imageDir + "\\" + listView1.Items[index].ImageKey;
+            pix.Save(toFile);
+
+            moveListToNext();
+        }
+
+        private void moveListToNext() {
+            if (listView1.Items.Count == 0) {
+                return;
+            }
+
+            int i = 0;
+            for (; i < listView1.Items.Count; i++) {
+                if (listView1.Items[i].Tag == null) {
+                    break;
+                }
+            }
+
+            // Check we didn't find anything
+            if (i == listView1.Items.Count) {
+                if (toldDone != true) {
+                    toldDone = true;
+                    MessageBox.Show("Thank you. Please zip up the extracted directory and send it back.", "Done!", MessageBoxButtons.OK);
+                }
+                return;
+            }
+
+            listView1.Items[i].Selected = true;
+            listView1.EnsureVisible(i);
+        }
+    }
+}
